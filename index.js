a668d2a59052fda0ddfe7a14c5ead87a43b28412
bxbao87 <bxbao18@apcs.vn>
2023-03-12 23:45:15 +0700

    [part3b] ex3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 morgan.token('body', function(req) {
     if (req.method === 'POST') 
@@ -9,6 +10,7 @@ morgan.token('body', function(req) {
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -92,7 +94,7 @@ app.post("/api/persons", (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
-    console.log("Start listening...")
+    console.log(`Server running on port ${PORT}`)
 })
