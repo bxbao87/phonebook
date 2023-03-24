b3c057e40880b751495e047c4f3c57a32d57ae3c
bxbao87 <bxbao18@apcs.vn>
2023-03-24 22:29:44 +0700

    [part3c] ex3.15

--- index.js
+++ index.js
@@ -12,37 +12,15 @@ morgan.token('body', function(req) {
 const app = express()
 const Person = require('./models/person')
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
 
 app.get("/api/persons", (request, response) => {
     Person.find({})
@@ -71,9 +49,14 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
 })
 
 app.post("/api/persons", (request, response) => {
@@ -105,6 +88,8 @@ app.post("/api/persons", (request, response) => {
             response.json(result)
         })
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=>{
